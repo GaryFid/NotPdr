61f963f98a469f39f976c526553fc079002ec714
HuanLampur <huanlampuria@gmail.com>
2025-06-23 23:03:10 +0300

    init: migrate to Next.js 10кф

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
 import './globals.css'
-import { Providers } from '../../app/providers'
+import { Providers } from '@/app/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
