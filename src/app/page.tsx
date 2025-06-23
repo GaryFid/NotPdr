80e6ecad6a52454a76005985a146d7e83ff56948
HuanLampur <huanlampuria@gmail.com>
2025-06-24 00:41:42 +0300

    init: migrate to Next.js 199

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
+'use client'
+
 import { MainMenu } from '../components/main_menu_component';
-import Head from 'next/head';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>P.I.D.R. - Card Game</title>
-        <link rel="stylesheet" href="/css/style.css" />
-      </Head>
-      <div className="container">
-        <MainMenu onNavigate={() => {}} />
-      </div>
-    </>
+    <div className="container">
+      <MainMenu onNavigate={() => {}} />
+    </div>
   );
 }
